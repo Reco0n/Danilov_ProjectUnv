7916bdfd0191b13ac3e3e2f566a8f8774ed9ae6f
Egor <124181347+Reco0n@users.noreply.github.com>
2026-09-08 13:26:49 +0300

    adding StartFactory()+ displayMenu() + main

--- Danilov_Laba№1.cpp
+++ Danilov_Laba№1.cpp
@@ -9,7 +9,7 @@ private:
     bool isRepair;
 
 public:
-    Pipe() : KmMark(""), length(0), diametr(0), isRepair(false){} //Настройки по умолцанию
+    Pipe() : KmMark(""), length(0), diametr(0), isRepair(false){} 
     
     void inputCons(){
         cout<<"";
@@ -98,6 +98,16 @@ public:
         cout<<""<<levelFactory;
     }
 
+    void StartFactory(){
+        if(workingFactories < amountFactories){
+            workingFactories++;
+            cout<<""<<workingFactories;
+        }
+        else{
+            cout<<"";
+        }
+    }
+
     void StopFactory(){
         if(workingFactories > 0){
             workingFactories --;
@@ -109,3 +119,111 @@ public:
     }
     
 };
+
+void displayManu(){
+    cout<<"1.Труба";
+    cout<<"2.КС";
+    cout<<"3.Просмотр";
+    cout<<"4.Редакт. трубы";
+    cout<<"5.Редакт. КС";
+    cout<<"0.Выход";
+    cout<<"Выберетие действие: ";
+}
+
+int main(){
+    Pipe pipe;
+    KC kc;
+    bool pipeExists = false;
+    bool kcExists = false;
+    int choice;
+    
+    while(true){
+        displayManu();
+        cin>>choice;
+
+        switch (choice){
+            case 0:{
+                cout<<"0";
+                return 0;
+            }
+
+            case 1:{
+                if (pipeExists){
+                    cout<<"(y/n): ";
+                    char ans;
+                    cin>>ans;
+                    if (ans !='y'){
+                        break;
+                    }
+                }
+                pipe.inputCons();
+                pipeExists = true;
+                cout<<"";
+                break;
+            }
+
+            case 2:{
+                if (kcExists){
+                    cout<<"(y/n): ";
+                    char ans;
+                    cin>>ans;
+                    if (ans !='y'){
+                        break;
+                    }
+                }
+                kc.inputCons();
+                kcExists = true;
+                cout<<"";
+                break;
+            }
+
+            case 3:{
+                if(pipeExists){
+                    pipe.display();
+                }
+                else{
+                    cout<<"";
+                }
+
+                if (kcExists){
+                    kc.display();
+                }
+                else{
+                    cout<<"";
+                }
+                break;
+            }
+
+            case 4:{
+                if (!pipeExists){
+                    cout<<"";
+                    break;
+                }
+                pipe.isWorking();
+                break;
+            }
+            case 5:{
+                if (!kcExists){
+                    cout<<"";
+                    break;
+                }
+                cout<<"1.";
+                cout<<"2.";
+                cout<<"выбор: ";
+                int subChoice;
+                cin>>subChoice;
+
+                switch (subChoice){
+                    case 1: kc.StartFactory();
+                    break;
+                    case 2:kc.StopFactory();
+                    break;
+                    default:cout<<"";
+                    break; 
+                }
+                break;
+            }
+        }
+
+    }
+}
